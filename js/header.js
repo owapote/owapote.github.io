b81b7a31cc732bb50fabe7b6608869fa38efe4a7
owpaote <michiru09141111@gmail.com>
2025-05-01 01:16:47 +0900

    add footer and split header and footer

--- js/header.js
+++ js/header.js
@@ -4,76 +4,92 @@ const SelectableLanguage = Object.freeze({
     TokiPona: "tokipona",
 });
 
-$(function(){
-    let language;
-    let isDayMode = true;
+//ヘッダーのボタンなどのイベントを追加
+function InitHeaderEvent(){
+    AddToChangeColorModeEvent();
+    AddToChangeLanguageEvent();
+}
 
-    language = localStorage.getItem("userLanguage");
-    if(language == null) language = SelectableLanguage.Japanese;
-    else $("#languageSelector").val(language);
+//カラーテーマの切り替え
+function AddToChangeColorModeEvent(){
+    $(function(){
+        let isDayMode = true;
 
-    $.ajax({
-        url: "./../json/topPage.json",
-        dataType: "json",
-        type: "GET",
-    }).done(function (data){
-        Translate(data);
-    });
-
-    let colorTheme = localStorage.getItem("userColorTheme");
-    if (colorTheme != null && colorTheme === "nightMode") {
-        isDayMode = false;
-        $("#colorThemeToggleButton").toggleClass("is-night");
-        $(document.body).addClass("darkMode");
-    }
-
-    setTimeout(function(){
-        $(document.body).addClass("enableTransition");
-    },100);
-
-    //言語設定
-    $("#languageSelector").change(function() {
-        let getLanguage = $("option:selected").val();
-        
-        if(Object.values(SelectableLanguage).includes(getLanguage)){
-            language = getLanguage;
-            localStorage.setItem("userLanguage",language);
-
-            $.ajax({
-                url: "./../json/topPage.json",
-                dataType: "json",
-                type: "GET",
-            }).done(function (data){
-                Translate(data);
-            });
-        }else{
-            alert("This language cannot selected:"+SelectableLanguage[getLanguage]);
+        //初期設定
+        let colorTheme = localStorage.getItem("userColorTheme");
+        if (colorTheme != null && colorTheme === "nightMode") {
+            isDayMode = false;
+            $("#colorThemeToggleButton").toggleClass("is-night");
+            $(document.body).addClass("darkMode");
         }
-    });
 
-    //カラーテーマの切り替え
-    $("#colorThemeToggleButton").on("click", function() {
-        isDayMode = !isDayMode;
-        $(this).toggleClass("is-night");
-        $(document.body).toggleClass("darkMode");
+        //ページが読み込まれた瞬間はtransitionをさせないようにしている
+        setTimeout(function(){
+            $(document.body).addClass("enableTransition");
+        },100);
 
-        if (isDayMode) {
-            localStorage.setItem("userColorTheme", "lightMode");
-        } else {
-            localStorage.setItem("userColorTheme", "nightMode");
-        }
-    });
+        //イベント
+        $("#colorThemeToggleButton").on("click", function() {
+            isDayMode = !isDayMode;
+            $(this).toggleClass("is-night");
+            $(document.body).toggleClass("darkMode");
 
-    //言語を変換する
-    Translate = (json) => {
-        $(document).ready(function () {
-            $("#headerText").text(json["headerText"][language]);
-            $("#topPageNav").text(json["topPageNav"][language]);
-            $("#discriptionNav").text(json["discriptionNav"][language]);
-            $("#interestListNav").text(json["interestListNav"][language]);
-            $("#contactFormNav").text(json["contactFormNav"][language]);
-            $("#contentText").text(json["contentText"][language]);
+            if (isDayMode) {
+                localStorage.setItem("userColorTheme", "lightMode");
+            } else {
+                localStorage.setItem("userColorTheme", "nightMode");
+            }
         });
-    }
-});
+    });
+}
 
+//言語設定
+function AddToChangeLanguageEvent(){
+    $(function(){
+        let language;
+
+        //初期設定
+        language = localStorage.getItem("userLanguage");
+        if(language == null) language = SelectableLanguage.Japanese;
+        else $("#languageSelector").val(language);
+        $.ajax({
+            url: "./../json/topPage.json",
+            dataType: "json",
+            type: "GET",
+        }).done(function (data){
+            Translate(data);
+        });
+
+        //イベント
+        $("#languageSelector").change(function() {
+            let getLanguage = $("option:selected").val();
+            
+            if(Object.values(SelectableLanguage).includes(getLanguage)){
+                language = getLanguage;
+                localStorage.setItem("userLanguage",language);
+
+                $.ajax({
+                    url: "./../json/topPage.json",
+                    dataType: "json",
+                    type: "GET",
+                }).done(function (data){
+                    Translate(data);
+                });
+            }else{
+                alert("This language cannot selected:"+SelectableLanguage[getLanguage]);
+            }
+        });
+
+        //jsonより、HTMLに挿入
+        Translate = (json) => {
+            $(document).ready(function () {
+                $("#headerText").text(json["headerText"][language]);
+                $("#topPageNav").text(json["topPageNav"][language]);
+                $("#discriptionNav").text(json["discriptionNav"][language]);
+                $("#interestListNav").text(json["interestListNav"][language]);
+                $("#contactFormNav").text(json["contactFormNav"][language]);
+                $("#contentText").text(json["contentText"][language]);
+            });
+        }
+    });
+}
