ebc38c577270c3d5156f9fe625c94f87a4d4f37d
owapote <michiru09141111@gmail.com>
2025-05-13 17:40:03 +0900

    add slideshow

--- js/topPage.js
+++ js/topPage.js
@@ -4,6 +4,7 @@ export class TopPage extends MainTemplate{
     ChangeContentsLanguage(){
         function TranslateByPage(json,language){
             $("#welcomeText").text(json["welcomeText"][language]);
+            $("#recentTopicsText").text(json["recentTopicsText"][language]);
         }
         super.ChangeLanguage(TranslateByPage,"./../json/topPage.json");
     }
